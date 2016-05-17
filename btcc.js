cdf265a58daa1ec4d8f48fef38d4707eb64b9cc4
Lu, Jun <luj1985@gmail.com>
2016-05-17 21:57:14 +0800

    remove simulation script, use "debug" module instead

--- btcc.js
+++ btcc.js
@@ -71,19 +71,7 @@ const CREATE_PURCHASE_ORDER_CONSTRAINT = {
   notificationURL: [ is.required(), is.isString() ]
 };
 
-/*
- * https://www.btcc.com/apidocs/justpay-payment-gateway-json-rpc-api
- *
- * 名称             类型   必选？ 描述
- * price           number  是   客户购买的商品价格.
- * currency        string  是   客户购买商品时使用的货币种类，可以是[CNY],[USD],[SGD],[MYR],[BTC].
- * notificationURL string  是   将订单状态返回给商家服务器的链接. 最大长度为255字符，必须以https开头.
- * returnURL       string  否   用户完成支付以后返回的页面链接. 最大长度为255字符. 注意：用户完成支付以后，生成的支付订单ID会以以下参数形式附在重定向链接后：?purchaseorder_id=.
- * externalKey     string  否   商家用来识别此购物订单的外部密钥. 可以是字母，数字以及下划线的组合，最大长度为255字符. 必须是唯一的.
- * itemDesc        string  否   向客户展示的其将要购买的商品描述信息.最大长度为64k字节.
- * phoneNumber     string  否   用户的手机号码. 客户在购买过程中需要使用手机来接收确认短信.
- * settlementType  number  否   商家收款方式，支持两种模式. 默认值为0，款项将以比特币的形式直接转到商家的毕加索钱包里; 值为1时，款项会以法币形式依照合同规定，扣除手续费后，定时转给商家. 当以法币形式支付时，最小的比特币支付限额为0.0001个.
-*/
+// https://www.btcc.com/apidocs/justpay-payment-gateway-json-rpc-api
 BTCC.prototype.createPurchaseOrder = function(args, callback) {
   const result = validator.validate(args, CREATE_PURCHASE_ORDER_CONSTRAINT);
   if (result === true) {
